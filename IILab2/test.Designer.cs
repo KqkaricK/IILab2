2dbeac070c12ab7f479a112c9c1bd6fae3293081
KqkaricK <andrew73-42@yandex.ru>
2021-09-09 19:34:38 +0700

    Добавьте файлы проекта.

---
+++ IILab2/test.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace IILab2
+{
+    partial class test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.no_button = new System.Windows.Forms.Button();
+            this.yes_button = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(99, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // no_button
+            // 
+            this.no_button.Location = new System.Drawing.Point(197, 141);
+            this.no_button.Name = "no_button";
+            this.no_button.Size = new System.Drawing.Size(75, 23);
+            this.no_button.TabIndex = 4;
+            this.no_button.Text = "НЕТ";
+            this.no_button.UseVisualStyleBackColor = true;
+            this.no_button.Click += new System.EventHandler(this.no_button_Click);
+            // 
+            // yes_button
+            // 
+            this.yes_button.Location = new System.Drawing.Point(18, 141);
+            this.yes_button.Name = "yes_button";
+            this.yes_button.Size = new System.Drawing.Size(75, 23);
+            this.yes_button.TabIndex = 3;
+            this.yes_button.Text = "ДА";
+            this.yes_button.UseVisualStyleBackColor = true;
+            this.yes_button.Click += new System.EventHandler(this.yes_button_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(284, 138);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 190);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.no_button);
+            this.Controls.Add(this.yes_button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "test";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Типо крутой тест";
+            this.Load += new System.EventHandler(this.test_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button no_button;
+        private System.Windows.Forms.Button yes_button;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ IILab2/test.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace IILab2
+{
+    public partial class test : Form
+    {
+        private Stopwatch stopWatch;
+        const int size = 50;
+        int[] num;
+        int master = 0;
+        string[] lines = File.ReadAllLines("data.txt");
+        string[] done = new string[size];
+        string time = DateTime.Now.ToString("d-M-yyyy HH-mm-ss");
+
+        public test()
+        {
+            InitializeComponent();
+        }
+
+        private void yes_button_Click(object sender, EventArgs e)
+        {
+            stopWatch.Stop();
+            File.AppendAllText("log" + time + ".txt", label2.Text + " Да " + label1.Text + Environment.NewLine);
+            if (master > 49)
+            {
+                MessageBox.Show("Тест заверщён!");
+                System.Windows.Forms.Application.Exit();
+            }
+            else 
+            {
+                stopWatch.Reset();
+                stopWatch.Start();
+                voprosi();
+            }
+        }
+
+        private void no_button_Click(object sender, EventArgs e)
+        {
+            stopWatch.Stop();
+            File.AppendAllText("log" + time + ".txt", label2.Text + " Нет " + label1.Text + Environment.NewLine);
+            if (master > 49)
+            {
+                MessageBox.Show("------------");
+                Application.Exit();
+            }
+            else
+            {
+                stopWatch.Reset();
+                stopWatch.Start();
+                voprosi();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.label1.Text = string.Format("{0:hh\\:mm\\:ss\\:fff}", stopWatch.Elapsed);
+        }
+
+        private void test_Load(object sender, EventArgs e)
+        {
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            num = poradok();
+            voprosi();
+        }
+        private int[] poradok()
+        {
+            int[] nums = new int[size];
+            int i = 0;
+            while (size > i)
+            {
+                nums[i] = i;
+                i++;
+            }
+            Random rand = new Random();
+            for (i = size - 1; i >= 1; i--)
+            {
+                int j = rand.Next(i + 1);
+                int tmp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = tmp;
+            }
+            return nums;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        void voprosi()
+        {
+            done[master] = lines[num[master]];
+            label2.Text = done[master];
+            master++;
+        }
+    }
+}
